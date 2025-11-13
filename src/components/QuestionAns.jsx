f9c64d48c0d33ffad0c57f4a4443b62a342ea953
jawadpathan1213-sys <jawadpathan1213@gmail.com>
2025-11-13 06:25:43 +0500

    Fix ESLint issues and update News component

--- src/components/QuestionAns.jsx
+++ src/components/QuestionAns.jsx
@@ -10,7 +10,7 @@ const QuestionAns = ({item, index }) => {
               > 
                 {item.type == "q" ? (
                   
-                  <li className=' text-right px-1 my-1  md:px-2 sm:py-1 py-0.3 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl border border-zinc-300 dark:border-zinc-700 bg-zinc-300 dark:bg-zinc-700 list-none wrap-break-word'>
+                  <li className=' text-right px-1 my-1  md:px-2 sm:py-1 py-0.3 rounded-tl-4xl rounded-br-4xl rounded-bl-4xl border border-zinc-300 dark:border-zinc-700 bg-zinc-300 dark:bg-zinc-700 list-none wrap-break-word'>
                     <Answers
                       ans={item.text}
                       index={0}
